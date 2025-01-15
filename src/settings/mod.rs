b30cc199d44c42efb483f27bf555d5dbe332bcac
HyperAlch <Daemonleak@protonmail.com>
2025-01-14 21:32:56 -0500

    Settings are now OnceLock<Mutex> global

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -4,6 +4,25 @@ use crate::settings::error::SettingsError;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
+use std::sync::{Mutex, OnceLock};
+pub(crate) static SETTINGS: OnceLock<Mutex<Settings>> = OnceLock::new();
+
+// Function to initialize the global Settings
+pub fn init_settings() {
+    // This will initialize the SETTINGS global variable once
+    SETTINGS
+        .set(Mutex::new(Settings::default()))
+        .unwrap_or_else(|_| {
+            panic!("Settings were already initialized");
+        });
+}
+
+// Function to get a thread-safe reference to the global Settings
+pub fn global_settings() -> &'static Mutex<Settings> {
+    SETTINGS
+        .get()
+        .expect("Settings should be initialized first")
+}
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Settings {
@@ -82,12 +101,53 @@ impl Settings {
 
 #[cfg(test)]
 mod tests {
-    use crate::settings::Settings;
+    use crate::settings::{global_settings, init_settings, Settings};
 
     #[test]
     fn test_save_and_load_settings() {
         test_save_settings();
         test_load_settings();
+        test_save_and_load_static_settings();
+    }
+    fn test_save_and_load_static_settings() {
+        init_settings(); // Initialize SETTINGS at the beginning
+
+        {
+            // Test saving settings
+            let settings = global_settings();
+            let mut writable_settings = settings
+                .lock()
+                .unwrap_or_else(|poisoned| poisoned.into_inner());
+
+            writable_settings.ocr_monitor_number = 0;
+            writable_settings.ocr_x = 0;
+            writable_settings.ocr_y = 0;
+            writable_settings.ocr_width = 5000;
+            writable_settings.ocr_height = 300;
+            writable_settings.icon_monitor_number = 0;
+            writable_settings.icon_x = 100;
+            writable_settings.icon_y = 100;
+            writable_settings.icon_width = 50;
+            writable_settings.icon_height = 50;
+
+            if let Err(e) = writable_settings.save() {
+                panic!("{:?}", e);
+            }
+        }
+
+        {
+            // Test loading settings
+            let settings = global_settings();
+            let mut writable_settings = settings
+                .lock()
+                .unwrap_or_else(|poisoned| poisoned.into_inner());
+
+            if let Err(e) = writable_settings.load() {
+                panic!("{:?}", e);
+            }
+
+            println!("{:?}", *writable_settings); // Log loaded settings
+        }
     }
 
     fn test_save_settings() {
@@ -95,7 +155,7 @@ mod tests {
             ocr_monitor_number: 0,
             ocr_x: 0,
             ocr_y: 0,
-            ocr_width: 500,
+            ocr_width: 600,
             ocr_height: 300,
 
             icon_monitor_number: 0,
