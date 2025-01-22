9675eaf785aec53df17c5c0b7a40586791913cc1
HyperAlch <Daemonleak@protonmail.com>
2025-01-22 00:25:13 -0500

    Refactor `capture_targets` to capture and save multiple image targets
    Updated the `capture_targets` test to include capturing and saving both OCR and icon target images. Consolidated settings management into a local structure.

--- src/screen_cap/mod.rs
+++ src/screen_cap/mod.rs
@@ -113,16 +113,15 @@ impl WindowList {
 
 mod tests {
     use crate::screen_cap::{capture_area_from_image, ScreenCap, WindowList};
+    use crate::settings;
     use crate::settings::{global_settings, init_settings};
 
     #[test]
-    fn capture_target() {
+    fn capture_targets() {
         init_settings();
-        let ocr_x: u32;
-        let ocr_y: u32;
-        let ocr_width: u32;
-        let ocr_height: u32;
 
+        // Get global settings, load into a local version
+        let mut local_settings = settings::Settings::default();
         {
             let settings = global_settings();
             let mut writable_settings = settings
@@ -133,22 +132,48 @@ mod tests {
                 panic!("{:?}", e);
             }
 
-            ocr_x = writable_settings.ocr_x;
-            ocr_y = writable_settings.ocr_y;
-            ocr_width = writable_settings.ocr_width;
-            ocr_height = writable_settings.ocr_height;
-        }
-        let all_windows = WindowList::new();
+            local_settings.ocr_x = writable_settings.ocr_x;
+            local_settings.ocr_y = writable_settings.ocr_y;
+            local_settings.ocr_width = writable_settings.ocr_width;
+            local_settings.ocr_height = writable_settings.ocr_height;
 
+            local_settings.icon_x = writable_settings.icon_x;
+            local_settings.icon_y = writable_settings.icon_y;
+            local_settings.icon_width = writable_settings.icon_width;
+            local_settings.icon_height = writable_settings.icon_height;
+        }
+
+        // Get target window
+        let all_windows = WindowList::new();
         let window = all_windows.filter_by_title("GW2");
         let window = window.get_vec();
-
         let window = ScreenCap::new(window.get(0).unwrap().1.clone());
+
+        // Get image from target window
         let mut image = window.target_window.capture_image().unwrap();
-        let captured_image =
-            capture_area_from_image(&mut image, ocr_x, ocr_y, ocr_width, ocr_height).unwrap();
-        captured_image
-            .save("./process-images/ocr_target.png")
-            .unwrap();
+
+        // Capture and save OCR image
+        let captured_ocr_image = capture_area_from_image(
+            &mut image,
+            local_settings.ocr_x,
+            local_settings.ocr_y,
+            local_settings.ocr_width,
+            local_settings.ocr_height,
+        )
+        .unwrap()
+        .save("./process-images/ocr_target.png")
+        .unwrap();
+
+        // Capture and save icon image
+        let captured_icon_image = capture_area_from_image(
+            &mut image,
+            local_settings.icon_x,
+            local_settings.icon_y,
+            local_settings.icon_width,
+            local_settings.icon_height,
+        )
+        .unwrap()
+        .save("./process-images/icon_target.png")
+        .unwrap();
     }
 }
